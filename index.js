90d2478bfcf8279f2504daed68587a06ded5b4ad
XadillaX <admin@xcoder.in>
2014-11-18 11:51:47 +0800

    let \t be space

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var spider = require("nodegrassex");
 var default_motto = "不自由，毋宁死。";
 
 exports.get = function(callback) {
-	spider.get("http://www.verycd.com/statics/title.saying?timestamp=" + Date.now() + Math.random(), function(data, status, respheader) {
-		if(status !== 200) {
-			data = default_motto;
-		} else {
-		    var reg = /new Array\(([\s\S]*?)\);.+/;
+    spider.get("http://www.verycd.com/statics/title.saying?timestamp=" + Date.now() + Math.random(), function(data, status, respheader) {
+        if(status !== 200) {
+            data = default_motto;
+        } else {
+            var reg = /new Array\(([\s\S]*?)\);.+/;
             var temp = reg.exec(data);
             if(temp.length !== 2) {
                 callback(default_motto);
@@ -31,8 +31,8 @@ exports.get = function(callback) {
             callback(array[idx]);
             return;
         }
-	}, "utf8").on("error", function(e) {
-		callback(default_motto);
-	});
+    }, "utf8").on("error", function(e) {
+        callback(default_motto);
+    });
 };
 
